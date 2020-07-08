3c08b30be3d59554d2dad2d40dc3aa95ff172385
Angelg10 <angelguzman97@gmail.com>
2020-07-08 16:01:18 -0400

    initial commit, room, player, some of REPL

--- src/player.py
+++ src/player.py
@@ -1,2 +1,11 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player:
+    def __init__(self, name, current_room):
+        self.name = name
+        self.current_room = current_room
+
+    def print_room(self):
+        print(f"Hello {self.name}, you're {self.current_room}")
